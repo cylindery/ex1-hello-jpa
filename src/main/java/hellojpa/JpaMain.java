e702c49450523e3405fbf8e9656d87951f6c844c
juwonju <jxwxnjx@gmail.com>
2023-03-31 15:37:00 +0900

    영속성 컨텍스트 2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,15 +17,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
 
-            //영속
-            System.out.println("=== Before ===");
-            em.persist(member);
-            System.out.println("=== After ===");
+            System.out.println("=============");
 
             tx.commit();
         } catch (Exception e) {
